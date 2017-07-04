392d96a61e668979a181f97602a18d9bd44fb67f
root <root@61949b3d2f95>
2017-07-04 00:35:05 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_RRxSUlTbjQ",
+  "homePage" : "Main",
+  "name" : "Auto_RRxSUlTbjQ",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
